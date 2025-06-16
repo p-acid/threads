50843ff25adf0bbabde719eae51a4c9464c6337d
p-acid <qkrtkstjd01@gmail.com>
2025-06-16 21:27:06 +0900

    style: add tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
