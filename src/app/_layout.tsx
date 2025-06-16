881fad7622b542cdaa766802656f0f7859d2271b
p-acid <qkrtkstjd01@gmail.com>
2025-06-16 21:34:18 +0900

    style: dark theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Slot } from "expo-router";
+import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+
+const theme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: "white",
+  },
+};
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <ThemeProvider value={theme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
